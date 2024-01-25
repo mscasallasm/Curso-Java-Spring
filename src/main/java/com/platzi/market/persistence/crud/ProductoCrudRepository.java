18e84e283af11615979e8b05a3c81cf19313a22e
mscasallasm <stiven_casallas@hotmail.com>
2024-01-25 11:11:37 -0500

    Clase 17: Query Methods

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,17 @@
 package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //findByIdCategoria es el Query method, lo que es simplificar @Query
+    //Esto también se puede combinar devolviendo la lista y colocando cualquier nombre al methodo ejemplo getByCategoria
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
+
 }
